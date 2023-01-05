e8e4184b0aa8ec189421feb223d9412737893382
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-05 17:18:29 +0300

    to_str

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -2,6 +2,6 @@
 """ type-annotated function add"""
 
 
-def to_str(n: float) -> int:
-    """Sums A and B"""
+def to_str(n: float) -> str:
+    """floating-point number to a string"""
     return str(n)
