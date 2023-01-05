e15898049cb7c7c36555679d579db658da46e32f
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-05 17:07:11 +0300

    Add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 """ type-annotated function add"""
 
 
-def add(a: float, b: float):
+def add(a: float, b: float) -> float:
     """Sums A and B"""
     return a+b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 """ type-annotated function add"""
 
 
-def concat(str1: str, str2: str):
+def concat(str1: str, str2: str) -> str:
     """concats str1 and str 2"""
     return str1 + str2
