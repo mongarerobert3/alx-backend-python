9f35e57c274a488e9ef71b5993ba9ce496442f0c
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-05 18:22:07 +0300

    list of floats

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""task 5"""
+'''Task 5's module.
+'''
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """list"""
+    '''Computes the sum of a list of floating-point numbers.
+    '''
     return float(sum(input_list))
